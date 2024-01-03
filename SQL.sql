94c72935ece1bb2385db9d9ae3f42bfbd0cc5570
Mila Podchasova <Pod4asova20@gmail.com>
2024-01-03 22:35:45 +0300

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -87,3 +87,64 @@ SELECT *
 FROM students
 WHERE id>140;
 
+ --20. Вывести пользователя, у которых id меньше 130.
+SELECT *
+FROM students
+WHERE id<130;
+
+ --21. Вывести пользователя, у которых id меньше 127 или больше 188.
+SELECT *
+FROM students
+WHERE id<127 or id>188;
+
+ --22. Вывести пользователя, у которых id меньше либо равно 137.
+SELECT *
+FROM students
+WHERE id<=137;
+
+ --23. Вывести пользователя, у которых id больше либо равно 137.
+SELECT *
+FROM students
+WHERE id>=137;
+
+ --24. Вывести пользователя, у которых id больше 180 но меньше 190.
+SELECT *
+FROM students
+WHERE id>180 AND id<190;
+
+ --25. Вывести пользователя, у которых id между 180 и 190.
+SELECT *
+FROM students
+WHERE id BETWEEN 180 AND 190;
+
+ --26. Вывести пользователей, где password равен 12333, 1m313, 123313.
+SELECT *
+FROM students
+WHERE password IN ('12333', '1m313', '123313');
+
+ --27. Вывести пользователей, где created_on равен 2020-10-03 00:00:00, 2021-05-19 00:00:00, 2021-03-26 00:00:00.
+SELECT *
+FROM students
+WHERE created_on IN ('2020-10-03 00:00:00', '2021-05-19 00:00:00', '2021-03-26 00:00:00');
+
+ --28. Вывести минимальный id.
+SELECT min(id) AS min_id
+FROM students;
+
+ --29. Вывести максимальный.
+SELECT max(id) AS max_id 
+FROM students;
+
+ --30. Вывести количество пользователей.
+SELECT count(id) AS students_number
+FROM students;
+
+ --31. Вывести id пользователя, имя, дату создания пользователя. Отсортировать по порядку возрастания даты добавления пользоватлеля.
+SELECT id, name, created_on
+FROM students
+ORDER BY created_on;
+
+ --32. Вывести id пользователя, имя, дату создания пользователя. Отсортировать по порядку убывания даты добавления пользоватлеля.
+SELECT id, name, created_on
+FROM students
+ORDER BY created_on DESC
